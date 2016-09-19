b89166900cc1644be21c9998ac1fccb9681eea26
JCHEN-Y410P\Johnny <johnnysanchen@gmail.com>
2016-09-19 19:57:38 -0400

    press and hold to charge the force for the ball

--- WordThrow/Assets/Scripts/Catapult.cs
+++ WordThrow/Assets/Scripts/Catapult.cs
@@ -5,11 +5,17 @@ public class Catapult : MonoBehaviour {
 
 	public Rigidbody projectile;
 	public Transform catapult;
-	public float force = 1000f;
-	public float moveSpeed = 10f;
+	public float baseForce = 800;
+	public float maxForce = 10000;
+	public float timeToMax = 1;
+	public float moveSpeed = 10;
+
+	private float _timePressed;
+	private float _timeReleased;
+	private float _force;
 
 	void Start () {
-		
+		_force = baseForce;
 	}
 	
 	void Update () {
@@ -18,10 +24,22 @@ public class Catapult : MonoBehaviour {
 
 		transform.Translate(new Vector3(x, y, 0));
 
+		if (Input.GetMouseButtonDown(0))
+		{
+			_timePressed = Time.time;
+		}
+
 		if (Input.GetMouseButtonUp(0))
 		{
+			_timeReleased = Time.time;
 			Rigidbody ball = (Rigidbody)Instantiate(projectile, catapult.position, catapult.rotation);
-			ball.AddForce(catapult.forward * force);
+			//Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			//catapult.position.Set(mousePos.x, mousePos.y, 0);
+			//Rigidbody ball = (Rigidbody)Instantiate(projectile, catapult.position, catapult.rotation);
+			_force += ((_timeReleased - _timePressed)/timeToMax)*maxForce;
+			if (_force > maxForce) { _force = maxForce; }
+			ball.AddForce(catapult.forward * _force);
+			_force = baseForce;
 		}
 	}
 }
