67448a6308c386c801ef04a5554fdcb788f9e023
Daniel Haynes <hayned2@gmail.com>
2016-09-20 20:14:35 -0400

    Added a timer script and object. The UI object needs to be moved to the proper place.

---
+++ WordThrow/Assets/Timer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text text;
+	public float game_time;
+
+	// Update is called once per frame
+	void Update () {
+		game_time -= Time.deltaTime;
+		text.text = game_time.ToString("F2") + "s";
+		if (game_time <= 0) {
+			//Load endgame screen.
+		}
+	}
+}
