e76f7e9f891744905b19cb7fa0dd8c1cf73df9d9
Daniel Haynes <hayned2@gmail.com>
2016-09-20 22:47:23 -0400

    Worked on the lightning code.

--- WordThrow/Assets/setLightByNegativity.cs
+++ WordThrow/Assets/setLightByNegativity.cs
@@ -10,22 +10,29 @@ public class setLightByNegativity : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		positive_color = new Color (255.0f/255, 242.0f/255, 208.0f/255);
-		negative_color = new Color (200.0f/255, 200.0f/255, 200.0f/255);
+		negative_color = new Color (200.0f/255, 150.0f/255, 200.0f/255);
+		StartCoroutine ("ChangeLighting");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Light> ().color = Color.Lerp (positive_color, negative_color, negativity);
+		if (Input.GetKeyDown (KeyCode.R)) {
+			negativity += 0.1f;
+		} else if (Input.GetKeyDown (KeyCode.T)) {
+			negativity -= 0.1f;
+		}
+		//GetComponent<Light> ().color = Color.Lerp (positive_color, negative_color, negativity);
 	}
 
 	IEnumerator ChangeLighting(){
 		yield return new WaitForSeconds (5f);
-		negativity = FindObjectOfType<GenerateLevel> ().GetPositivity();
+		//negativity = FindObjectOfType<GenerateLevel> ().GetPositivity();
+		Color current_color = GetComponent<Light> ().color;
 		float timer = 3f;
 		while (timer > 0) {
 			timer -= Time.deltaTime;
 			float percentage = timer / 3f;
-			//LERP The Lighting.
+			Color.Lerp (current_color, Color.Lerp(positive_color, negative_color, negativity), percentage);
 			yield return new WaitForEndOfFrame();
 		}
 		StartCoroutine ("ChangeLighting");
